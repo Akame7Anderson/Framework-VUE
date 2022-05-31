969d41a1a8b4afb1489025d1fcb725e84f6c59f3
Akame <andersonrobertopetry@gmail.com>
2022-05-31 14:19:32 -0300

    navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
+    <!--<v-navigation-drawer
       v-model="drawer"
       app
     >
-      <!--  -->
+        
+    </v-navigation-drawer>-->
+    <v-navigation-drawer 
+    v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
