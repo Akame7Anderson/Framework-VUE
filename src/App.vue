df5e68b66aa59e6a7905620f88e2d6916bf7324c
Akame <andersonrobertopetry@gmail.com>
2022-06-01 17:18:23 -0300

    designando caminho para os "items"

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@
         <v-list-item
           v-for="item in items"
           :key="item.title"
+          :to="item.to"
           link
         >
           <v-list-item-icon>
@@ -50,7 +51,7 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view/>
     </v-main>
   </v-app>
 </template>
@@ -58,8 +59,8 @@
 <script>
   export default {
     data: () => ({ drawer: null, items: [
-          { title: 'Tarefas', icon: 'mdi-view-dashboard' },
-          { title: 'Sobre', icon: 'mdi-help-box' },
+          { title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sobre', icon: 'mdi-help-box', to:'/sobre' },
         ], }),
   }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Tarefas',
+    component: () => import('../views/Tarefas.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/sobre',
+    name: 'Sobre',
+    
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
